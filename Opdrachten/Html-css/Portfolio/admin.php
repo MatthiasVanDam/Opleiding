6b5a11dd47e0af38308842d1dc9a5268c210a2f8
MatthiasVanDam <matthias.1991@live.be>
2020-10-08 15:39:04 +0200

    .

---
+++ Opdrachten/Html-css/Portfolio/admin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/30128c7d4f.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Montserrat:500,700" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="media.css">
+    <title>Matthias Van Dam</title>
+</head>
+
+<body>
+
+    <!--BG IMAGE-->
+
+
+    <!--Nav-->
+    <nav>
+        <!--Container-->
+        <div class="container d-flex between">
+            <div class="navBar">
+                <a href="#">Home</a>
+                <a href="#myWork">My Work</a>
+                <a href="bestanden/CV.pdf" target="_blank">My CV</a>
+                <a href="#contactMe">Contact</a>
+            </div>
+            <div class="links">
+                <a href="https://www.linkedin.com/in/matthias-van-dam-b997b7178/" target="_blank"><i
+                        class="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/MatthiasVanDam" target="_blank"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+        <!--ContainerEND-->
+    </nav>'
+
+    <!-- Login -->
+    
+
+
+
+    <!--Footer-->
+    <footer>
+        <div class="container">
+            <p>copyright &copy; 2020 -All rights reserved | Designed by Matthias Van Dam</p>
+        </div>
+
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+        </script>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ Opdrachten/Jobdag/app.js
@@ -0,0 +1,26 @@
+//Code by Arno De Koninck
+
+$("#sunIcon, #moonIcon").on("click", function() {
+    console.log($(this).attr("id"));
+    if ($(this).attr("id") == "moonIcon") {
+        $(":root").attr("data-theme", "dark");
+        $("#logo").addClass("dark");
+    } else {
+        $(":root").removeAttr("data-theme");
+        $("#logo").removeClass("dark");
+    }
+    if ($(this).hasClass("selectedThemeIcon") == false) {
+        $("#sunIcon, #moonIcon").removeClass("selectedThemeIcon");
+        $(this).toggleClass("selectedThemeIcon");
+    }
+});
+
+
+document.getElementById('cursBtn').onclick = function() {
+    var el = document.getElementById('cursistenContainer');
+    if (el.style.display != 'block') {
+        el.style.display = 'block';
+    } else {
+        el.style.display = 'none';
+    };
+};
